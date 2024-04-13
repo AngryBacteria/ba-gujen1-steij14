6a784848b17d39b0d9937277237e845ae116dcf1
AngryBacteria <AngryBacteria@protonmail.com>
2024-04-13 17:08:09 +0200

    external ner script

--- research/datacorpus/corpus/ggponc2.py
+++ research/datacorpus/corpus/ggponc2.py
@@ -4,10 +4,11 @@ import os.path
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
+from research.datacorpus.corpus.utils_ner import parse_ner_dataset
 from research.logger import logger
 
-fine_annotations_folder = "F:\\OneDrive - Berner Fachhochschule\\Dokumente\\UNI\\Bachelorarbeit\\datensets\\ggponc2\\json\\fine"
-fine_annotations_ner_folder = "F:\\OneDrive - Berner Fachhochschule\\Dokumente\\UNI\\Bachelorarbeit\\datensets\\ggponc2\\conll\\fine"
+fine_annotations_folder = "F:\\OneDrive - Berner Fachhochschule\\Dokumente\\UNI\\Bachelorarbeit\\datensets\\corpus\\ggponc2\\json\\fine"
+fine_annotations_ner_folder = "F:\\OneDrive - Berner Fachhochschule\\Dokumente\\UNI\\Bachelorarbeit\\datensets\\corpus\\ggponc2\\conll\\fine"
 
 
 def load_json(filename):
@@ -26,52 +27,27 @@ def get_ggponc_json():
     return data_short, data_long
 
 
-def load_conll(filepath):
-    """Load the CoNLL file and return the sentences as a list of dictionaries"""
-    with open(filepath, "r", encoding="utf-8") as file:
-        text = file.read()
-
-    sentences = []
-    # split the text into the data chunks and init empty lists
-    lines = text.strip().split("\n")
-    words = []
-    ner_tags = []
-    for line in lines:
-        if line.strip():  # This skips empty lines, they are used to separate sentences
-            word, ner_tag = line.split()
-            words.append(word)
-            ner_tags.append(ner_tag)
-        else:  # End of sentence is reached
-            if words:
-                sentences.append({"words": words, "ner_tags": ner_tags})
-                words = []
-                ner_tags = []
-
-    logger.debug(f"Parsed {len(sentences)} GGPONC NER annotations from file {filepath}")
-    return sentences
-
-
 def get_ggponc_ner():
     """Load the GGPOC NER data from the fine annotations folder and return the short and long documents."""
     # get and combine short data
-    data_short_dev = load_conll(
+    data_short_dev = parse_ner_dataset(
         os.path.join(fine_annotations_ner_folder, "short", "dev_fine_short.conll")
     )
-    data_short_test = load_conll(
+    data_short_test = parse_ner_dataset(
         os.path.join(fine_annotations_ner_folder, "short", "test_fine_short.conll")
     )
-    data_short_train = load_conll(
+    data_short_train = parse_ner_dataset(
         os.path.join(fine_annotations_ner_folder, "short", "train_fine_short.conll")
     )
     data_short_ner = data_short_dev + data_short_test + data_short_train
     # get and combine long data
-    data_long_dev = load_conll(
+    data_long_dev = parse_ner_dataset(
         os.path.join(fine_annotations_ner_folder, "long", "dev_fine_long.conll")
     )
-    data_long_test = load_conll(
+    data_long_test = parse_ner_dataset(
         os.path.join(fine_annotations_ner_folder, "long", "test_fine_long.conll")
     )
-    data_long_train = load_conll(
+    data_long_train = parse_ner_dataset(
         os.path.join(fine_annotations_ner_folder, "long", "train_fine_long.conll")
     )
     data_long_ner = data_long_dev + data_long_test + data_long_train
