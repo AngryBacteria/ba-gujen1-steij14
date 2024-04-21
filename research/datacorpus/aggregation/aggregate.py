b3a65bd78e9c98a917f83b98de63a005bbbcb47e
AngryBacteria <AngryBacteria@protonmail.com>
2024-04-21 10:33:56 +0200

    read our dataset as huggingface dataset :)

--- research/datacorpus/aggregation/aggregate.py
+++ research/datacorpus/aggregation/aggregate.py
@@ -1,4 +1,6 @@
 # TODO: aggregation of all corpus datasets
+from datasets import load_dataset
+
 from research.datacorpus.aggregation.agg_bronco import (
     get_all_simple_bronco_prompts,
     get_all_bronco_normalization_prompts,
@@ -7,18 +9,23 @@ from research.datacorpus.aggregation.agg_ggponc import get_all_simple_ggponc_pro
 import pandas as pd
 
 
-def save_all_prompts(normalization=True):
+def save_all_prompts(bronco=True, ggponc=True, normalization=True):
     prompts = []
-    ggponc_prompts = get_all_simple_ggponc_prompts()
-    simple_bronco_prompts = get_all_simple_bronco_prompts()
-    if normalization:
-        bronco_normalization_prompts = get_all_bronco_normalization_prompts()
-    else:
-        bronco_normalization_prompts = []
-
-    prompts.extend(ggponc_prompts)
-    prompts.extend(simple_bronco_prompts)
-    prompts.extend(bronco_normalization_prompts)
+    if ggponc:
+        ggponc_prompts = get_all_simple_ggponc_prompts()
+        prompts.extend(ggponc_prompts)
+    if bronco:
+        simple_bronco_prompts = get_all_simple_bronco_prompts()
+        prompts.extend(simple_bronco_prompts)
+        if normalization:
+            bronco_normalization_prompts = get_all_bronco_normalization_prompts()
+            prompts.extend(bronco_normalization_prompts)
 
     prompts_df = pd.DataFrame(prompts)
-    prompts_df.to_csv("prompts.csv", index=False)
+    prompts_df.to_csv("prompts.csv", index=False, header=["text"])
+
+
+dataset = load_dataset("csv", data_files={"data": "prompts.csv"})[
+    "data"
+].train_test_split(test_size=0.1, shuffle=True, seed=42)
+print(dataset)
