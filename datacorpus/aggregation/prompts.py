a6605931d763b99ff5069eaa2bb18ac0634c9aa3
AngryBacteria <AngryBacteria@protonmail.com>
2024-05-05 19:37:03 +0200

    new cardio prompt

--- datacorpus/aggregation/prompts.py
+++ datacorpus/aggregation/prompts.py
@@ -9,20 +9,19 @@ SYSTEM_PROMPT_NORMALIZATION = "Du bist ein fortgeschrittener Algorithmus, der da
 MEDICATION_INSTRUCTION = """Extrahiere alle Medikamente aus dem folgenden Text. Falls keine im Text vorkommen, schreibe "Keine vorhanden":
 
 <<CONTEXT>>"""
-MEDICATION_INSTRUCTION_LEVEL_OF_TRUTH = """Extrahiere alle Medikamente aus dem folgenden Text. Für jedes extrahierte Medikament, füge in eckigen Klammern an, ob das Medikament in Bezug auf den Patienten positiv [POSITIV], negativ [NEGATIV], spekulativ [SPEKULATIV] oder zukünftig [ZUKÜNFTIG] ist. Falls keine im Text vorkommen, schreibe "Keine vorhanden":
+MEDICATION_INSTRUCTION_LEVEL_OF_TRUTH = """Extrahiere alle Medikamente aus dem folgenden Text. Für jedes extrahierte Medikament, füge in eckigen Klammern an, ob das Medikament in Bezug auf den Patienten positiv [POSITIV], negativ [NEGATIV], spekulativ [SPEKULATIV] oder zukünftig [ZUKÜNFTIG] ist. Falls keine Medikamente im Text vorkommen, schreibe "Keine vorhanden":
 
 <<CONTEXT>>"""
 MEDICATION_NORMALIZATION_INSTRUCTION = """Weise dem im Text genannten Medikament „<<ENTITY>>“ den entsprechenden ATC-Code zu:
 
 <<CONTEXT>>"""
-
-MEDICATION_INSTRUCTION_ATTRIBUTES = """Extrahiere alle Medikamente aus folgenden Text. Für jedes extrahierte Medikament, füge zusätzlich gefundene Attribute in einer eckigen Klammer hinzu. Falls keine vorhanden sind, schreibe "Keine Attribute vorhanden":
+MEDICATION_INSTRUCTION_ATTRIBUTES = """Extrahiere alle Medikamente aus folgenden Text. Für jedes extrahierte Medikament, füge falls vorhanden in eckigen Klammern die FREQUENZ und DOSIERUNG des Medikaments hinzu. Falls beides nicht vorhanden ist, füge leere eckige Klammern ein. Falls keine Medikamente im Text vorhanden sind, schreibe "Keine vorhanden":
 
 <<CONTEXT>>"""
 
 
 # DIAGNOSIS PROMPTS
-DIAGNOSIS_INSTRUCTION = """Extrahiere alle Diagnosen und Symptome aus dem folgenden Text. Für jedes Symptom oder Diagnose, füge in eckigen Klammern an, ob das Medikament in Bezug auf den Patienten positiv [POSITIV], negativ [NEGATIV], spekulativ [SPEKULATIV] oder zukünftig [ZUKÜNFTIG] ist. Falls keine im Text vorkommen, schreibe "Keine vorhanden":
+DIAGNOSIS_INSTRUCTION = """Extrahiere alle Diagnosen und Symptome aus dem folgenden Text. Für jedes Symptom oder Diagnose, füge in eckigen Klammern an, ob das Medikament in Bezug auf den Patienten positiv [POSITIV], negativ [NEGATIV], spekulativ [SPEKULATIV] oder zukünftig [ZUKÜNFTIG] ist. Falls keine Diagnosen oder Symptome im Text vorkommen, schreibe "Keine vorhanden":
 
 <<CONTEXT>>"""
 DIAGNOSIS_INSTRUCTION_LEVEL_OF_TRUTH = """Extrahiere alle Diagnosen und Symptome aus dem folgenden Text. Falls keine im Text vorkommen, schreibe "Keine vorhanden":
@@ -37,7 +36,7 @@ DIAGNOSIS_NORMALIZATION_INSTRUCTION = """Weise dem im Text genannten Symptom ode
 TREATMENT_INSTRUCTION = """Extrahiere alle klinischen Prozeduren aus dem folgenden Text. Falls keine im Text vorkommen, schreibe "Keine vorhanden":
 
 <<CONTEXT>>"""
-TREATMENT_INSTRUCTION_LEVEL_OF_TRUTH = """Extrahiere alle klinischen Prozeduren aus dem folgenden Text. Für jede Prozedur, füge in eckigen Klammern an, ob das Medikament in Bezug auf den Patienten positiv [POSITIV], negativ [NEGATIV], spekulativ [SPEKULATIV] oder zukünftig [ZUKÜNFTIG] ist. Falls keine im Text vorkommen, schreibe "Keine vorhanden":
+TREATMENT_INSTRUCTION_LEVEL_OF_TRUTH = """Extrahiere alle klinischen Prozeduren aus dem folgenden Text. Für jede Prozedur, füge in eckigen Klammern an, ob das Medikament in Bezug auf den Patienten positiv [POSITIV], negativ [NEGATIV], spekulativ [SPEKULATIV] oder zukünftig [ZUKÜNFTIG] ist. Falls keine klinischen Prozeduren im Text vorkommen, schreibe "Keine vorhanden":
 
 <<CONTEXT>>"""
 TREATMENT_NORMALIZATION_INSTRUCTION = """Weise der im Text genannten klinischen Prozedur „<<ENTITY>>“ den entsprechenden OPS-2017 Code zu:
