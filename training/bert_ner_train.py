d8ab093c137c3594d36051dd13def49681906708
AngryBacteria <AngryBacteria@protonmail.com>
2024-05-12 14:24:44 +0200

    fix compute metrics not working correctly

--- training/bert_ner_train.py
+++ training/bert_ner_train.py
@@ -137,7 +137,13 @@ def compute_metrics(p: EvalPrediction):
         [LABEL_LIST[l] for (p, l) in zip(prediction, label) if l != -100]
         for prediction, label in zip(predictions, labels)
     ]
-    return seqeval.compute(predictions=true_predictions, references=true_labels)
+    results = seqeval.compute(predictions=true_predictions, references=true_labels)
+    return {
+        "precision": results["overall_precision"],
+        "recall": results["overall_recall"],
+        "f1": results["overall_f1"],
+        "accuracy": results["overall_accuracy"],
+    }
 
 
 tokenized_dataset = dataset.map(
